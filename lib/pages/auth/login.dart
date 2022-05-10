7f894c42b8441250bc7d564ff2c9248d14c6fe46
Ricardo-MT <63120247+Ricardo-MT@users.noreply.github.com>
2022-05-10 11:08:40 +0200

    scrolling

--- lib/pages/auth/login.dart
+++ lib/pages/auth/login.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:egresadoapp/utils/palette.dart';
 import 'package:egresadoapp/widgets/footer/privacyfooter.dart';
 import 'package:egresadoapp/widgets/spacer/spacer.dart';
@@ -13,95 +15,102 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Expanded(
-              child: Container(
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("assets/images/login.png"),
-                    fit: BoxFit.cover)),
-            child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Image.asset(
-                    "assets/images/egresadologopng.png",
-                    height: 120,
+      body: SingleChildScrollView(
+        child: ConstrainedBox(
+          constraints: BoxConstraints.tightFor(
+              height: max(700, MediaQuery.of(context).size.height)),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                  child: Container(
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/images/login.png"),
+                        fit: BoxFit.cover)),
+                child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Image.asset(
+                        "assets/images/egresadologopng.png",
+                        height: 120,
+                      ),
+                    )),
+              )),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: SizedBox(
+                  width: 500,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 320,
+                        child: Column(
+                          children: [
+                            spacerS,
+                            Text(
+                              "BIENVENID@",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontSize: 35,
+                                  color: MuiPalette.FONT,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            spacerXL,
+                            MuiInput(
+                              label: "EMAIL",
+                              onChanged: (p0) {
+                                // credentials.username = p0 ?? "";
+                              },
+                              onFieldSubmitted: (_) {
+                                // handleLogin(context, userProvider);
+                              },
+                              inputType: TextInputType.text,
+                            ),
+                            spacerS,
+                            MuiInput(
+                              label: "CONTRASEÑA",
+                              onChanged: (p0) {
+                                // credentials.username = p0 ?? "";
+                              },
+                              onFieldSubmitted: (_) {
+                                // handleLogin(context, userProvider);
+                              },
+                              inputType: TextInputType.text,
+                            ),
+                            spacerXL,
+                            MuiButton(
+                              onPressed: () async {
+                                // handleLogin(context, userProvider);
+                              },
+                              text: "ENTRAR",
+                              variant: MuiButtonVariant.CONTAINED,
+                            ),
+                            spacerXL,
+                            MuiButton(
+                              onPressed: () {
+                                Navigator.of(context)
+                                    .pushNamed(Routes.register);
+                              },
+                              text: "¿Aún no tienes cuenta?",
+                              variant: MuiButtonVariant.LINK,
+                            ),
+                            spacerS,
+                          ],
+                        ),
+                      ),
+                      const PrivacyFooter()
+                    ],
                   ),
-                )),
-          )),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: SizedBox(
-              width: 500,
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 320,
-                    child: Column(
-                      children: [
-                        spacerS,
-                        Text(
-                          "BIENVENID@",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontSize: 35,
-                              color: MuiPalette.FONT,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        spacerXL,
-                        MuiInput(
-                          label: "EMAIL",
-                          onChanged: (p0) {
-                            // credentials.username = p0 ?? "";
-                          },
-                          onFieldSubmitted: (_) {
-                            // handleLogin(context, userProvider);
-                          },
-                          inputType: TextInputType.text,
-                        ),
-                        spacerS,
-                        MuiInput(
-                          label: "CONTRASEÑA",
-                          onChanged: (p0) {
-                            // credentials.username = p0 ?? "";
-                          },
-                          onFieldSubmitted: (_) {
-                            // handleLogin(context, userProvider);
-                          },
-                          inputType: TextInputType.text,
-                        ),
-                        spacerS,
-                        MuiButton(
-                          onPressed: () async {
-                            // handleLogin(context, userProvider);
-                          },
-                          text: "ENTRAR",
-                          variant: MuiButtonVariant.CONTAINED,
-                        ),
-                        spacerXL,
-                        MuiButton(
-                          onPressed: () {
-                            Navigator.of(context).pushNamed(Routes.register);
-                          },
-                          text: "¿Aún no tienes cuenta?",
-                          variant: MuiButtonVariant.LINK,
-                        ),
-                        spacerS,
-                      ],
-                    ),
-                  ),
-                  const PrivacyFooter()
-                ],
-              ),
-            ),
-          )
-        ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/pages/auth/register.dart
+++ lib/pages/auth/register.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 import '../../router/router.dart';
@@ -13,106 +15,112 @@ class RegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: SizedBox(
-              width: 500,
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 320,
-                    child: Column(
-                      children: [
-                        spacerS,
-                        Text(
-                          "ÚNETE A LA COMUNIDAD",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontSize: 35,
-                              color: MuiPalette.FONT,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        spacerXL,
-                        MuiInput(
-                          label: "EMAIL",
-                          onChanged: (p0) {
-                            // credentials.username = p0 ?? "";
-                          },
-                          onFieldSubmitted: (_) {
-                            // handleLogin(context, userProvider);
-                          },
-                          inputType: TextInputType.text,
-                        ),
-                        spacerS,
-                        MuiInput(
-                          label: "CONTRASEÑA",
-                          onChanged: (p0) {
-                            // credentials.username = p0 ?? "";
-                          },
-                          onFieldSubmitted: (_) {
-                            // handleLogin(context, userProvider);
-                          },
-                          inputType: TextInputType.text,
-                        ),
-                        spacerS,
-                        MuiInput(
-                          label: "CONFIRMA TU CONTRASEÑA",
-                          onChanged: (p0) {
-                            // credentials.username = p0 ?? "";
-                          },
-                          onFieldSubmitted: (_) {
-                            // handleLogin(context, userProvider);
-                          },
-                          inputType: TextInputType.text,
-                        ),
-                        spacerXL,
-                        MuiButton(
-                          onPressed: () async {
-                            // handleLogin(context, userProvider);
-                          },
-                          text: "REGÍSTRATE",
-                          variant: MuiButtonVariant.CONTAINED,
-                        ),
-                        spacerXL,
-                        MuiButton(
-                          onPressed: () {
-                            Navigator.of(context).pushNamed(Routes.login);
-                          },
-                          text: "¿Ya tienes cuenta?",
-                          variant: MuiButtonVariant.LINK,
-                        ),
-                        spacerS,
-                      ],
-                    ),
-                  ),
-                  const PrivacyFooter()
-                ],
-              ),
-            ),
-          ),
-          Expanded(
-              child: Container(
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("assets/images/register.png"),
-                    fit: BoxFit.cover)),
-            child: Align(
-                alignment: Alignment.bottomCenter,
+      body: SingleChildScrollView(
+        child: ConstrainedBox(
+          constraints: BoxConstraints.tightFor(
+              height: max(700, MediaQuery.of(context).size.height)),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(
+                width: 500,
                 child: Padding(
                   padding: const EdgeInsets.all(20.0),
-                  child: Image.asset(
-                    "assets/images/egresadologopng.png",
-                    height: 120,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 320,
+                        child: Column(
+                          children: [
+                            spacerS,
+                            Text(
+                              "ÚNETE A LA COMUNIDAD",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontSize: 35,
+                                  color: MuiPalette.FONT,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            spacerXL,
+                            MuiInput(
+                              label: "EMAIL",
+                              onChanged: (p0) {
+                                // credentials.username = p0 ?? "";
+                              },
+                              onFieldSubmitted: (_) {
+                                // handleLogin(context, userProvider);
+                              },
+                              inputType: TextInputType.text,
+                            ),
+                            spacerS,
+                            MuiInput(
+                              label: "CONTRASEÑA",
+                              onChanged: (p0) {
+                                // credentials.username = p0 ?? "";
+                              },
+                              onFieldSubmitted: (_) {
+                                // handleLogin(context, userProvider);
+                              },
+                              inputType: TextInputType.text,
+                            ),
+                            spacerS,
+                            MuiInput(
+                              label: "CONFIRMA TU CONTRASEÑA",
+                              onChanged: (p0) {
+                                // credentials.username = p0 ?? "";
+                              },
+                              onFieldSubmitted: (_) {
+                                // handleLogin(context, userProvider);
+                              },
+                              inputType: TextInputType.text,
+                            ),
+                            spacerXL,
+                            MuiButton(
+                              onPressed: () async {
+                                // handleLogin(context, userProvider);
+                              },
+                              text: "REGÍSTRATE",
+                              variant: MuiButtonVariant.CONTAINED,
+                            ),
+                            spacerXL,
+                            MuiButton(
+                              onPressed: () {
+                                Navigator.of(context).pushNamed(Routes.login);
+                              },
+                              text: "¿Ya tienes cuenta?",
+                              variant: MuiButtonVariant.LINK,
+                            ),
+                            spacerS,
+                          ],
+                        ),
+                      ),
+                      const PrivacyFooter()
+                    ],
                   ),
-                )),
-          )),
-        ],
+                ),
+              ),
+              Expanded(
+                  child: Container(
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/images/register.png"),
+                        fit: BoxFit.cover)),
+                child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Image.asset(
+                        "assets/images/egresadologopng.png",
+                        height: 120,
+                      ),
+                    )),
+              )),
+            ],
+          ),
+        ),
       ),
     );
   }
